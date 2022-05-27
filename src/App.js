63140a29a1465afd2b16fc4211208606aa93410f
Nelly <$>
2022-05-27 18:05:06 -0400

    finally got api connected yay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-const api ={
-  key: "47faa98c00b88628615e4d2eb7a2d9d9",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+import React, { useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=47faa98c00b88628615e4d2eb7a2d9d9`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+  
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -22,7 +34,7 @@ function App() {
     <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="City name..." />
+          <input type="text" className="search-bar" placeholder="City name..." onChange={event => setLocation(event.target.value)} onKeyPress={searchLocation} value={location} />
         </div>
 
           <div className="location-box">
