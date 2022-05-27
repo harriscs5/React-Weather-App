c774d934a4255c2b0b3f4aaf79f6450fde780d3f
Nelly <$>
2022-05-27 13:32:21 -0400

    added background images to folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+const api ={
+  key: "47faa98c00b88628615e4d2eb7a2d9d9",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-   
+   Hello
     </div>
   );
 }
