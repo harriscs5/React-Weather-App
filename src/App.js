f9b849342a52cfb4ae5bfed79b9a1bc3f85cfeb5
Nelly <$>
2022-05-27 21:58:00 -0400

    pulled all needed info from api

--- src/App.js
+++ src/App.js
@@ -38,14 +38,36 @@ function App() {
         </div>
 
           <div className="location-box">
-              <div className="location"> New York City, US</div>
+              <div className="location">{data.name}</div>
               <div className="date">{dateBuilder(new Date())}</div> 
           </div>
 
           <div className="weather-box">
-            <div className="temp">15°</div>
-            <div className='icon'><img src="https://openweathermap.org/img/wn/04n.png" alt=""/></div>
-            <div className="weather">Sunny</div>
+            <div className="temp">{data.main ? <h1>{data.main.temp}°F</h1> : null}
+</div>
+            
+
+
+            <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
+
+          <div className="bottom">
+            <div className="feels">
+            {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+
+            <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
+              <p>Humidity</p>
+            </div>
+
+            <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
       
       </main>
